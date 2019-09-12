8fc985a9cc129a992ab1539e632d2976ac0e8f5d
l <liliw1@uchicago.edu>
2019-09-12 12:01:46 -0400

    Update the generation of Poisson distribution
    change the sample size used in the generation of theoretical poisson density line

--- plot_challege.R
+++ plot_challege.R
@@ -1,3 +1,6 @@
+setwd("D:/2019 UChi/MBL/BSD-QBio5/tutorials/reproducibility/data")
+library(tidyr)
+
 set.seed(1)
 
 data1 <- read.csv('cole_arthropod_data_1946.csv', header=T)
@@ -11,19 +14,19 @@ par(mfrow=c(1,1))
 spider <- apply(data1, 1, function(x) rep(x[1], x[2])) %>% unlist()
 hist(spider, freq = F, main = 'Distribution of spider')
 lines(density(spider), col="red")
-lines(density(rpois(100, lambda['spider'])), col=4)
+lines(density(rpois(colSums(data1)[2], lambda['spider'])), col=4)
 legend(1, 1, legend=c('Data', 'Poisson'), col=c(2, 4), lty=1)
 
 ###Plot for sowbug
 sowbug <- apply(data1, 1, function(x) rep(x[1], x[3])) %>% unlist()
 hist(sowbug, freq = F, main = 'Distribution of sowbug')
 lines(density(sowbug), col="red")
-lines(density(rpois(100, lambda['sowbug'])), col=4)
+lines(density(rpois(colSums(data1)[3], lambda['sowbug'])), col=4)
 legend(8, 0.2, legend=c('Data', 'Poisson'), col=c(2, 4), lty=1)
 
 ###Plot for weevil
 weevil <- apply(data2, 1, function(x) rep(x[1], x[2])) %>% unlist()
 hist(weevil, freq = F, main = 'Distribution of weevil')
 lines(density(weevil), col="red")
-lines(density(rpois(100, lambda['weevil'])), col=4)
+lines(density(rpois(colSums(data2)[2], lambda['weevil'])), col=4)
 legend(0.3, 4, legend=c('Data', 'Poisson'), col=c(2, 4), lty=1)
