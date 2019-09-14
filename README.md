38625d67564224c01919cba5070bd57798083387
Rob Rodriguez <rrodriguez4@uchicago.edu>
2019-09-14 13:29:51 -0400

    Finished!
    README.md was written in its entirety and plots cleaned up with lambda2 values for LGP Distribution comparison in Q5 and Q6 as indicated in the book. This should hopefully suffice.

--- README.md
+++ README.md
@@ -5,4 +5,18 @@ The distribution of spiders, sowbugs, and weevil eggs are compared to Poisson di
 
 A random distribution expects a Poisson distribution, however over-dispersed distribution expects a better fit with an LGP lambda2 greather than zero, and a under-dispered distribution expects a better fit with an LGP lambda2 less than zero. 
 
-The results are as follows:
+The results are as follows-
+
+Q1: Upon plotting, the distribution of spiders per board appears to be randomly distributed as the data follow the Poisson Distribution closely.
+
+Q2: Upon plotting, the distribution of sowbugs per board does not appear to be randomly distributed as the data fails to closely follow the Poisson Distribution closely.
+
+Q3: Upon plotting, the distribution of weevils per bean appears to be randomly distributed as the data follows the Poisson Distribution relatively closely, but not quite as tightly as observed in Q1.
+
+Q4: Upon plotting the Lagrangian Poission Distributon with a λ1 = calculated mean of spiders per board and λ2 of 0 (as indicated on page 11 of the "Reproducibility" section of the QBio book),  the Lagrangian Poisson Distribution corroborates the indication of random distribution seen by plotting the Poisson Distribution in Q1.
+
+Q5: Upon plotting the Lagrangian Poisson Distribution with λ1 = calculated mean of the sowbugs per board * (1-λ2) and λ2 = 0.53214for the deviation from expected the Poisson Dispersion (as indicated on page 11 of the "Reproducibility" section of the QBio book), the Lagrangian Poisson Distribution appears to be a slightly better fit to the data which failed to closely follow the Poisson Distribution in Q2, thus once again indicating that the distribution of sowbugs per board does not appear to be random.
+
+Q6: Upon plotting the Lagrangian Poisson Distribution with λ1 = calculated mean of weevils per bean * (1-λ2) and λ2 = 0.1 as an eyeballed value of deviation from the Poisson Dispersion (as indicated on page 11 of the "Reproducibility" section of the QBio book), the Lagrangian Poisson Distribution corroborates the indication of a random distribution seen by plotting the Poisson Distribution in Q3. 
+
+Important note: The value of λ2 in both Q5 and Q6 are greater than zero, suggesting that the sowbugs, and somewhat for the weevils, are over-dispersed and may be attracting greater numbers of individuals within environments (boards and beans) to cluster outside what would be expected by chance alone. 
--- plot_challege.R
+++ plot_challege.R
@@ -1,50 +1,51 @@
 set.seed(1)
 ##remember to set your working directory according to where you have folder with the data
-
 setwd("~/Desktop/QBio Bootcamp/BSD-QBio5/tutorials/reproducibility/data")
-tryCatch({library(RMKdiscrete)}, error=function(mes){
-  install.packages("RMKdiscrete")
-})
-tryCatch({library(ggplot2)}, error=function(mes){
-  install.packages("ggplot2")
-})  
-
+library(ggplot2)
 data1 <- read.csv('cole_arthropod_data_1946.csv', header=T)
 data2 <- read.csv("mitchell_weevil_egg_data_1975.csv", header=T)
 ##Q1 Poisson distribution for spider
-
-poisson <- function(species){
+library(ggplot2)
 lambda <- apply(data1, 2, function(x) sum(x*data1[, 1])/sum(x))[-1]
 names(lambda) <- c('spider', 'sowbug')
-lambda[species]
-max_spiders <- max(data1$k_number_of_arthropods)
-spider_poisson <- dpois(x=0:max_spiders, lambda[species])
+lambda['spider']
+
+spider_poisson <- dpois(x=0:17, lambda['spider'])
 sum_spider = sum(data1$C_count_of_boards_with_k_spiders)
 
 p_spider <- ggplot(data1,aes(x=k_number_of_arthropods))
 p_spider <- p_spider + geom_line(aes(y=spider_poisson),color="red") + 
   geom_point(aes(y=C_count_of_boards_with_k_spiders/sum_spider),color="blue")
 p_spider 
-}
-
-poisson("spider")
 
 ##As we can see, the distribution of spiders is very close to a Poisson distribution
 ##which implies a random distribution of spiders.
 
 #Q2 
-poisson("sowbug")
+lambda <- apply(data1, 2, function(x) sum(x*data1[, 1])/sum(x))[-2]
+names(lambda) <- c("spider", "sowbug")
+lambda["sowbug"]
+
+
+sowbug_poisson <- dpois(x=0:17, lambda["sowbug"])
+sum_sowbug <- sum(data1$C_count_of_boards_with_k_sowbugs)
+
+p_sowbug <- ggplot(data1,aes(x=k_number_of_arthropods))
+p_sowbug <- p_sowbug + geom_line(aes(y=sowbug_poisson),color="red") + 
+  geom_point(aes(y=C_count_of_boards_with_k_sowbugs/sum_sowbug),color="blue")
+p_sowbug
+
 ##As seen in calling p_sowbuyg, the distribution of sowbugs per board is not close to
 ##a Poisson distribution, suggesting a non-random distribution of sowbugs.
 
 ##Q3 Poisson Distribution for Weevil 
+library(ggplot2)
 weevil_data <- read.csv("mitchell_weevil_egg_data_1975.csv", header=T)
 weevil_data$bean_number_per_egg <- weevil_data$k_number_of_eggs*weevil_data$C_count_of_beans_with_k_eggs
 lambda_weevil <- sum(weevil_data$bean_number_per_egg)/sum(weevil_data$C_count_of_beans_with_k_eggs)
 nrow(weevil_data)
 names(weevil_data)
-max_weevils <- max(data2$k_number_of_eggs)
-weevil_poisson <- dpois(x=0:max_weevils, lambda_weevil)
+weevil_poisson <- dpois(x=0:4, lambda_weevil)
 sum_weevil <-sum(weevil_data$C_count_of_beans_with_k_eggs)
 p_weevil_egg <- ggplot(weevil_data, aes(x = k_number_of_eggs))+
   geom_line(aes(y = weevil_poisson), color = "yellow3") +
@@ -55,28 +56,36 @@ print(p_weevil_egg)
 #suggesting a random distribution of weevils per bean
 
 ##Q4 LGP distribution for spider
-
-spider_LGP <- dLGP(x=0:max_spiders,theta=lambda['spider'],lambda=0)
+install.packages("RMKdiscrete")
+library(RMKdiscrete)
+library(ggplot2)
+spider_LGP <- dLGP(x=0:17,theta=lambda['spider'],lambda=0)
 p_spider2 <- ggplot(data1,aes(x=k_number_of_arthropods))
-p_spider2 <- p_spider2 + geom_line(aes(y=spider_LGP),color="green") 
+p_spider2 <- p_spider2 + geom_line(aes(y=spider_LGP),color="green") +
+  geom_point(aes(y=C_count_of_boards_with_k_spiders/sum_spider),color="blue")
 p_spider2
 ##As λ2 = 0, the LGP distribution is completely the same as the original Poisson distribution
 ##the two lines completely overlap
 
 ##Q5 LGP distribution for sowbug
-sowbug_LGP <-dLGP(x=0:max_sowbugs,theta=lambda['sowbug'],lambda=0)
+install.packages("RMKdiscrete")
+library(RMKdiscrete)
+empirically_determined_lambda2 <- 0.53214
+lambda1_sowbug <- lambda["sowbug"] (1 - empirically_determined_lambda2)
+sowbug_LGP <-dLGP(x=0:17,theta=lambda1_sowbug,lambda=empirically_determined_lambda2)
 
 p_sowbug2 <- ggplot(data1, aes(x = k_number_of_arthropods))
-p_sowbug2 <-p_sowbug2 + geom_line(aes(y=sowbug_LGP),color="green")
+p_sowbug2 <-p_sowbug2 + geom_line(aes(y=sowbug_LGP),color="green") +
+  geom_point(aes(y=C_count_of_boards_with_k_sowbugs/sum_sowbug),color="blue")
 p_sowbug2
 
 #Q6 distribution of weevil
-weevil_LGP <- dLGP(x=0:max_sowbugs, theta = lambda_weevil, lambda=0)
+install.packages("RMKdiscrete")
+library(RMKdiscrete)
+weevil_lamb2 <- 0.1
+weevil_lamb1 <- lambda_weevil*(1 - weevil_lamb2)
+weevil_LGP <- dLGP(x=0:4, theta = lambda_weevil, lambda=0)
 p_weevil_egg2 <- ggplot(weevil_data, aes(x = k_number_of_eggs)) 
-p_weevil_egg2 <- p_weevil_egg2 + geom_line(aes(y=weevil_LGP),color = "green")
+p_weevil_egg2 <- p_weevil_egg2 + geom_line(aes(y=weevil_LGP),color = "green") +
+  geom_point(aes(y = C_count_of_beans_with_k_eggs/sum_weevil), color = "blue")
 p_weevil_egg2
-
-#the overlay between the Poisson distribution seen in question 5 and 6 are equivalent which means that it shows random distribution and reproducibility
-
-
-p_sowbug2
